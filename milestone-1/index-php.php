291f6204e17bbc904b47ae4ba12f08face8f2952
hexeva <patrick79.g@gmail.com>
2022-01-26 14:51:28 +0100

    first commit

---
+++ milestone-1/index-php.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP DISC</title>
+</head>
+<body>
+    <!-- HEADER -->
+    <header>
+       <div class="logo">
+           <img src="img/logo.png" alt="logo spotify">
+       </div>
+   </header>
+    <!-- HEADER -->
+
+    <!-- MAIN -->
+    <div class="main-app">
+        <div class="container">
+            <div class="row row-cols-5">
+                <div class="main-card">
+            
+                    <!-- SINGLE CARD -->
+                    <div class="single_card">
+                        <div class="my_banner">
+                            <img src="" alt="">
+
+                            <div class="banner_title">
+                                <h2>titolo</h2>
+                                <h3>autore</h3>
+                                <h4>anno</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- SINGLE CARD -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- MAIN -->
+  
+</body>
+</html>
